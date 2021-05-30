77188f0cba119e55d71280bae8a02018aeea2463
VanshajGoel <60982919+VanshajGoel@users.noreply.github.com>
2021-05-30 23:55:49 +0530

    Day-5/2-Codes

---
+++ Day5.java
@@ -0,0 +1,82 @@
+//Question 1 Link--> https://www.hackerrank.com/challenges/java-int-to-string/problem
+import java.util.*;
+import java.security.*;
+public class Solution {
+ public static void main(String[] args) {
+
+  DoNotTerminate.forbidExit();
+
+  try {
+   Scanner in = new Scanner(System.in);
+   int n = in .nextInt();
+   in.close();
+   //String s=???; Complete this line below
+    String s= Integer.toString(n);
+   //Write your code here
+    
+   
+   if (n == Integer.parseInt(s)) {
+    System.out.println("Good job");
+   } else {
+    System.out.println("Wrong answer.");
+   }
+  } catch (DoNotTerminate.ExitTrappedException e) {
+   System.out.println("Unsuccessful Termination!!");
+  }
+ }
+}
+
+//The following class will prevent you from terminating the code using exit(0)!
+class DoNotTerminate {
+
+ public static class ExitTrappedException extends SecurityException {
+
+  private static final long serialVersionUID = 1;
+ }
+
+ public static void forbidExit() {
+  final SecurityManager securityManager = new SecurityManager() {
+   @Override
+   public void checkPermission(Permission permission) {
+    if (permission.getName().contains("exitVM")) {
+     throw new ExitTrappedException();
+    }
+   }
+  };
+  System.setSecurityManager(securityManager);
+ }
+}
+
+//Question 2 link-->https://www.hackerrank.com/challenges/java-strings-introduction/problem
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        System.out.println(A.length()+B.length());
+        if(A.compareTo(B)>0)
+        {
+            System.out.println("Yes");
+        }
+        else
+        {
+            System.out.println("No");
+        }
+        String f1=A.substring(0,1);
+        String l1=A.substring(1,A.length());
+        String f2=B.substring(0,1);
+        String l2=B.substring(1,B.length());
+        f1=f1.toUpperCase();
+        f2=f2.toUpperCase();
+        String n1=f1+l1;
+        String n2=f2+l2;
+        System.out.println(n1+" "+n2);
+    }
+}
