4276ceb6e69dc6b47aa0d2639b9f135a5e22162c
VanshajGoel <60982919+VanshajGoel@users.noreply.github.com>
2021-06-07 23:57:14 +0530

    Day-13/1-Code

---
+++ Day13.py
@@ -0,0 +1,23 @@
+#Question 1-->https://www.hackerrank.com/challenges/python-mod-divmod/problem
+a=int(input())
+b=int(input())
+print(a//b)
+print(a%b)
+print(divmod(a,b))
+
+#Question 2-->https://www.hackerrank.com/challenges/python-power-mod-power/problem
+
+a=int(input())
+b=int(input())
+c=int(input())
+print(a**b)
+print(pow(a,b,c))
+
+#Question 3-->https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+e=a**b+c**d
+print(e)
