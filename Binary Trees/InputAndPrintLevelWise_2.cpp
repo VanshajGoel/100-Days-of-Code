b180ea9b7b2f2c61943a7be9d32a8aef8f7bb1cf
Vanshaj Goel <60982919+VanshajGoel@users.noreply.github.com>
2022-01-17 18:44:17 +0530

    Printing and Taking Input Level Wise

---
+++ Binary Trees/InputAndPrintLevelWise_2.cpp
@@ -0,0 +1,123 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+template<typename T>
+class BTNode
+{
+public:
+    T data;
+    BTNode* left;
+    BTNode* right;
+    BTNode(T data)
+    {
+        this->data=data;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+BTNode<int>* takeInputLevelWise()
+{
+    int rootData;
+    cout<<"Enter data: ";
+    cin>>rootData;
+    if(rootData==-1)
+        return NULL;
+    BTNode<int>* root = new BTNode<int>(rootData);
+    queue<BTNode<int>*> q;
+    q.push(root);
+    while(!q.empty())
+    {
+        BTNode<int>* h= q.front();
+        q.pop();
+        int leftData, rightData;
+        cout<<"Enter left child of "<<h->data<<" : ";
+        cin>>leftData;
+        if(leftData != -1)
+        {
+            BTNode<int>* l = new BTNode<int>(leftData);
+            h->left=l;
+            q.push(l);
+        }    
+        cout<<"Enter right child of "<<h->data<<" : ";
+        cin>>rightData;
+
+        if(rightData != -1)
+        {
+            BTNode<int>* r = new BTNode<int>(rightData);
+            h->right=r; 
+            q.push(r);
+        }      
+    }
+    return root;
+}
+
+void printTree(BTNode<int>* root)
+{
+    if(root==NULL)
+        return;
+    queue<BTNode<int>*>q;
+    q.push(root);
+    while(!q.empty())
+    {
+        BTNode<int>* h = q.front();
+        q.pop();
+        cout<<h->data<<" L:";
+        if(h->left!=NULL)
+        {
+            cout<<h->left->data;
+             q.push(h->left);
+        }
+        else
+            cout<<-1;
+       if(h->right!=NULL)
+        {
+            cout<<" R:"<<h->right->data<<endl;
+             q.push(h->right);
+        }
+        else
+            cout<<-1<<endl;
+
+        
+    }
+}
+
+void printTreeLevelWise(BTNode<int>* root)
+{
+    queue<BTNode<int>*>q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty())
+    {
+        BTNode<int>* h=q.front();
+        q.pop();
+        if(h==NULL)
+        {
+             cout<<endl;
+            if(!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+            
+        else
+        {
+        cout<<h->data<<" ";    
+        if(h->left)
+            q.push(h->left);
+        if(h->right)
+            q.push(h->right);   
+        }
+            
+    }
+}
+
+//1 2 3 4 5 6 7 -1 - 1 -1 -1 8 9 -1 -1 -1 -1 -1 -1
+int main()
+{
+    BTNode<int>* root= takeInputLevelWise();
+    printTreeLevelWise(root);
+    return 0;
+}
