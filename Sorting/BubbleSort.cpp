eb620d0460f349561184dd69a63b1621cd3e7daf
Vanshaj Goel <60982919+VanshajGoel@users.noreply.github.com>
2022-03-17 18:55:24 +0530

    Bubble sort

---
+++ Sorting/BubbleSort.cpp
@@ -0,0 +1,38 @@
+
+ #include <iostream>
+ using namespace std;
+ 
+void BubbleSort(int *arr, int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+
+ int main()
+ {
+     int n;
+     cout<<"Enter size of array: ";
+     cin>>n;
+     int arr[n];
+     cout <<"Enter elements of array:";
+     for(int i=0;i<n;i++)
+     {
+        cin>>arr[i];         
+     }
+    BubbleSort(arr,n);
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+ 
+ }
