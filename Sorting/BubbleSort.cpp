5f906134d4eaec336c78d4862d3aeb8622d221e1
Vanshaj Goel <60982919+VanshajGoel@users.noreply.github.com>
2022-03-17 22:23:03 +0530

    Bubble sort optimised

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -1,18 +1,21 @@
-
- #include <iostream>
- using namespace std;
+#include <iostream>
+using namespace std;
  
 void BubbleSort(int *arr, int n)
 {
     for(int i=0;i<n-1;i++)
     {
+        int flag = 0;
         for(int j=0;j<n-1;j++)
         {
             if(arr[j]>arr[j+1])
             {
                 swap(arr[j],arr[j+1]);
+                flag=1;
             }
         }
+        if(flag==0)
+            break;
     }
 }
 
